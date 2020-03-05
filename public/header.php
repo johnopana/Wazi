784c461640ad10a75b1b17496103ddb78f5b44a5
John <johnopana2016@gmail.com>
2020-03-05 10:57:54 +0300

    add files'

--- public/header.php
+++ public/header.php
@@ -10,4 +10,44 @@ body{
    <tr>
        <td width = "90" valign ="top">
  <!--You can modify the text, color,size,number of loops and more on the flash header by  editing the ext file (fence.txt) included in the zip file.--> 
- <div align = "left">
+ <div align="left"><object classid=clsid:D27CDB6E-AE6D-11CF-96B8-444553540000
+ codebase=http://download.micromedia.com/pub/shockwave/cabs/flash/swflash.cab#version = 5,0,2,0
+ width = 500
+ height=68>
+ <param name=movie value=image.awf>
+ <param name=quality value=height>
+ <param name=BGCOLOR value=#0000>
+ <param name=SCALE value=showall>
+ <param name=wmode value=transparent>
+ <embed src=image/fence.swf
+ quality=heigh
+ pluginspage=http://www.macromedia.com/shockwave/download/index.cgi?P1_Prod_version=shockwaveFlash type=
+ application/x-shockwave-flash
+ width=1365
+ height=68
+ background-color=#1d91d0;
+ scale = showall>
+ </embed>
+-</object></div></td>
+     <td width="100%">
+
+    </tr>
+-</table>
+  <table width="100%">
+  <tr>
+  <td>
+  <?php @$_SESSION['login'];
+  error_reporting(1);
+  ?>
+  </td>
+    <td>
+    <?php
+    if(isset($_SESSION['login']));
+    {
+        echo "<div align=\"right\"><strong><a href=\"index.php\">Home </a> |<a href=\"
+
+    }
+
+
+
+
