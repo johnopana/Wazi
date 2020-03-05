377f05831bc7383a36c2474f89f1bf621d3a939e
John <johnopana2016@gmail.com>
2020-03-05 09:57:41 +0300

    made changes

---
+++ public/header.php
@@ -0,0 +1,13 @@
+<style type="text/css">
+<! --
+body{
+    margin-left:0px;
+    margin-top 0px;
+}
+
+</style>
+<table border="0" width="100%" cellspacing="0" cellpadding="0" background ="image/topbkj.jpg">
+   <tr>
+       <td width = "90" valign ="top">
+ <!--You can modify the text, color,size,number of loops and more on the flash header by  editing the ext file (fence.txt) included in the zip file.--> 
+ <div align = "left">
